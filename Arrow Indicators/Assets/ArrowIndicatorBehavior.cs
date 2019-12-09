24419c7af91e7349a8c80fa7fcc4d5d8dd1869a5
Alexander <Orzescu>
2019-12-09 12:33:16 -0700

    Arrow Indicators

---
+++ Arrow Indicators/Assets/ArrowIndicatorBehavior.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowIndicatorBehavior : MonoBehaviour {
+
+    #region Public Variables
+
+    [Range(1, 10)]
+    public float positionOffset = 2;
+    public float minAlphaRange;
+    public float maxAlphaRange;
+    public bool isVisible;
+    
+    #endregion
+
+    #region Private Variables
+
+    [SerializeField]
+	private GameObject targetObject;
+    [SerializeField]
+	private GameObject playerObject;
+	private GameObject targetObjectWithOffset;
+    private Vector3 targetSpawnPoint;
+	private Vector3 originToTarget;
+    
+    #endregion
+
+    #region Unity Callbacks
+    void Start () {
+
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+		targetObjectWithOffset = new GameObject ("offsetPosition");
+
+	}
+
+	void Update () {
+
+		//Vector Math for pointing the arrow towards the target Bumbletum
+		originToTarget = targetObject.transform.position - playerObject.transform.position;
+
+        //Set the position of the indicator arrow on the parent, then offset it so that it "floats" around the parent
+        this.gameObject.transform.position = gameObject.transform.parent.position + (originToTarget.normalized * positionOffset);
+
+        //This is a modifier so that the arrow's don't point to a new y position. They'll always point perpendicular to the parent object.
+		float PlayerObjectsYPos = gameObject.transform.position.y;
+
+        float SameTargetXPos = targetObject.transform.position.x;
+        float SameTargetZPos = targetObject.transform.position.z;
+        targetObjectWithOffset.transform.position = new Vector3(SameTargetXPos, PlayerObjectsYPos, SameTargetZPos);
+
+        //Point to object
+		this.gameObject.transform.LookAt (targetObjectWithOffset.transform.position);
+
+	}
+
+    public void OnTriggerEnter(Collider otherObject)
+    {
+        if (otherObject.gameObject.tag == "Bumbletum")
+        {
+            targetObject = otherObject.gameObject;
+            Debug.Log("Wow look at this : " + otherObject.gameObject);
+        }
+    }
+    #endregion
+}
---
+++ Arrow Indicators/Assets/ArrowIndicatorManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowIndicatorManager : MonoBehaviour {
+
+    #region Public Variables
+
+    public GameObject PlayerObject;
+    public GameObject IndicatorPrefab;
+
+	[SerializeField]
+	public GameObject[] BumbleTumObjects;
+    public Transform[] locations;
+    public GameObject[] arrowIndicators;
+
+    #endregion
+
+    #region Unity Callbacks
+
+    void Start () {
+
+        //For now all the bumbletum objects are defined on start. Adding and subtracting bumbletums to the array of bumbletums around you needs to be done at a later date. 
+
+        for (int i = 0; i < BumbleTumObjects.Length; i++)
+        {
+            //Debug.Log("Bumbletum object " + i + " is named " + BumbleTumObjects[i].name);
+            locations[i] = BumbleTumObjects[i].gameObject.transform;
+
+            GameObject arrowIndic = Instantiate(IndicatorPrefab, locations[i].transform.position, Quaternion.identity) as GameObject;
+            arrowIndicators[i] = arrowIndic;
+            arrowIndicators[i].transform.parent = PlayerObject.transform;
+            //Debug.Log(arrowIndicators[i]);
+            //Debug.Log("The transform of object " + i + " is :" + locations[i]);
+        }
+    }
+
+    void Update () {
+
+        //TODO: Update the list of bumbletums around the player
+
+        //TODO: Add + subtract bumbletums from the indicator list 
+
+	}
+   
+    #endregion
+}
+
+
+
